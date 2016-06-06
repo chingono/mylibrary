b1df0694f2f447ab1bc0c237a25886e43a233268
Brian Chingono <chingono@chicagobooth.edu>
2016-06-05 20:47:12 -0500

    Lists can now be created

--- app/models/list.rb
+++ app/models/list.rb
@@ -1,4 +1,8 @@
 class List < ActiveRecord::Base
   belongs_to :user , :class_name => "User", :foreign_key => "user_id"
   belongs_to :article , :class_name => "Article", :foreign_key => "article_id"
+
+  validates :user, :presence => true
+  validates :user, :presence => true
+  
 end
