a80d1cd2f7b40ca4e18dc8d0a133fef178820b51
Brian Chingono <chingono@chicagobooth.edu>
2016-06-07 15:34:51 -0500

    Articles are now linked by tags

--- app/models/topic.rb
+++ app/models/topic.rb
@@ -1,3 +1,4 @@
 class Topic < ActiveRecord::Base
   has_many :taggings , :class_name => "Tagging", :foreign_key => "topic_id"
+  has_many :tagged_articles, :through => :taggings, :source => :article
 end
